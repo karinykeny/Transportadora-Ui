8efa388b8524d71ff7753afd7bf82b375bdf1f2a
karinydantas <karinykeny@gmail.com>
2020-02-15 17:20:32 -0300

    componente footer

--- src/app/components-generics/components-generics.module.ts
+++ src/app/components-generics/components-generics.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class ComponentsGenericsModule {}
