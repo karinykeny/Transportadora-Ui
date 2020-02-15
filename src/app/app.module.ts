f1fb604ba301bbf142378d763cb416e1b51b094a
karinydantas <karinykeny@gmail.com>
2020-02-15 18:51:31 -0300

    Modulo side-bar e multi-cards (estrutura)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ComponentsGenericsModule } from "./components-generics/components-generics.module";
+import { SideBarModule } from "./modules/side-bar/side-bar.module";
+import { MultiCardsModule } from "./modules/multi-cards/multi-cards.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [ComponentsGenericsModule, BrowserModule, AppRoutingModule],
+  imports: [
+    MultiCardsModule,
+    SideBarModule,
+    ComponentsGenericsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
